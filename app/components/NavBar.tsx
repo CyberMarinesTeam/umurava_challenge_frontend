de3e5620782965a1f7e486e815b00287bf1edd1a
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2025-01-17 15:00:39 +0200

    adding more content

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -3,7 +3,12 @@ import React from "react";
 const NavBar = () => {
   return (
     <nav className="h-[96px] border-b-[1px] items-center justify-center flex flex-row space-x-[100px]">
-      <img src="/umuravaLogo.webp" alt="umurava logo" className="w-[120px]" />
+      <img
+        src="/umuravaLogo.webp"
+        draggable="false"
+        alt="umurava logo"
+        className="w-[120px]"
+      />
       <ul className="flex  flex-row text-[13px] space-x-[40px]">
         <li className="text-[#2B71f0]">Home</li>
         <li>Challenge & Hackathons</li>
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,22 @@
 import NavBar from "./components/NavBar";
 
 export default function Home() {
+  const courseNames = [
+    "UI/UX Design",
+    "Data Science",
+    "Graphic Design",
+    "Data Analysis & Research",
+    "Animation",
+    "Videography & Photography",
+    "AI & Machine Learning",
+    "Web3",
+    "Digital Marketing & Communications",
+  ];
   return (
-    <div className="">
+    <main className="">
       <NavBar />
 
-      <main className="flex   flex-row items-center justify-center space-x-[10px] mt-[50px] ">
+      <section className="flex pb-[50px]   flex-row items-center justify-center space-x-[10px] mt-[50px] ">
         <div className="flex flex-col space-y-[50px] text-left w-[450px]">
           <h2 className="text-[#2B71f0] font-bold text-[30px]">
             Build Work Experience through Skills Challenges Program
@@ -22,10 +33,11 @@ export default function Home() {
         <img
           src="/homePagePhoto.webp"
           className="h-[400px] w-auto"
+          draggable="false"
           alt="home image"
         />
-      </main>
-      <main className="flex flex-col space-y-[20px] pb-[50px] bg-[#F9FAFB] items-center pt-[50px] justify-center">
+      </section>
+      <section className="flex flex-col space-y-[20px] pb-[50px] bg-[#F9FAFB] items-center pt-[50px] justify-center">
         <h2 className="text-[#03192E] text-[30px] font-bold">
           Experience a New Way of Building Work Experience
         </h2>
@@ -37,6 +49,7 @@ export default function Home() {
           <div className="bg-[#2B71F0] space-y-[30px] rounded-[10px] w-full p-[30px] flex flex-col">
             <img
               src="/bagLogo.webp"
+              draggable="false"
               className="w-[60px] rounded-[10px] h-auto"
               alt="bag logo"
             />
@@ -55,6 +68,7 @@ export default function Home() {
             <div className="bg-[#2B71F0] space-y-[30px]  rounded-[10px] w-full p-[30px] flex flex-col">
               <img
                 src="/bagLogo.webp"
+                draggable="false"
                 className="w-[60px] rounded-[10px] h-auto"
                 alt="bag logo"
               />
@@ -69,6 +83,7 @@ export default function Home() {
             <div className="bg-[#2B71F0]  space-y-[30px] rounded-[10px] w-full p-[30px] flex flex-col">
               <img
                 src="/bagLogo.webp"
+                draggable="false"
                 className="w-[60px] rounded-[10px] h-auto"
                 alt="bag logo"
               />
@@ -83,7 +98,46 @@ export default function Home() {
             </div>
           </div>
         </div>
-      </main>
-    </div>
+      </section>
+      <section className="flex items-center p-[70px] justify-center">
+        <div className="bg-[url('/cardBg.webp')] text-white bg-cover bg-center h-[200px] space-x-[70px] rounded-[30px] w-[850px] flex flex-row items-center justify-center">
+          <div className="grid place-items-center grid-cols-1">
+            <span className="font-bold text-[20px]">1</span>
+            <span>year</span>
+          </div>
+          <div className="grid place-items-center grid-cols-1">
+            <span className="font-bold text-[20px]">500+</span>
+            <span>Challenges Completed</span>
+          </div>
+          <div className="grid place-items-center grid-cols-1">
+            <span className="font-bold text-[20px]">10k+</span>
+            <span>users</span>
+          </div>
+          <div className="grid place-items-center grid-cols-1">
+            <span className="font-bold text-[20px]">6+</span>
+            <span>Countries</span>
+          </div>
+        </div>
+      </section>
+      <section className="flex flex-col space-y-[20px] pb-[50px] items-center pt-[50px] justify-center">
+        <h2 className="text-[#03192E] text-[30px] w-[700px] text-center font-bold">
+          Skills Challenges Cover various in-demand skills and Careers for the
+          digital economy{" "}
+        </h2>
+        <p className="text-gray-300 text-[13px] w-[580px] text-center">
+          Explore the projects that various talents are working on{" "}
+        </p>
+        <div className="flex flex-wrap justify-center gap-[20px] w-[700px] ">
+          {courseNames.map((courseName: string, index) => (
+            <button
+              key={index}
+              className="bg-[#f1f1f1] hover:bg-[#2b71f0] rounded-[10px] text-[#657888] p-[14px] transition-all duration-150 hover:text-white"
+            >
+              {courseName}
+            </button>
+          ))}
+        </div>
+      </section>
+    </main>
   );
 }
