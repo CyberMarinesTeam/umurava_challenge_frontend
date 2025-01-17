7868a6a516390925420436e29aa39901ace139c6
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2025-01-17 13:59:11 +0200

    adding more content

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -2,16 +2,16 @@ import React from "react";
 
 const NavBar = () => {
   return (
-    <nav className="h-[96px] border-b-[1px] items-center justify-center flex">
-      <img src="/umuravaLogo.webp" alt="umurava logo" className="w-[100px]" />
-      <ul className="flex  flex-row text-[13px]">
-        <li>Home</li>
+    <nav className="h-[96px] border-b-[1px] items-center justify-center flex flex-row space-x-[100px]">
+      <img src="/umuravaLogo.webp" alt="umurava logo" className="w-[120px]" />
+      <ul className="flex  flex-row text-[13px] space-x-[40px]">
+        <li className="text-[#2B71f0]">Home</li>
         <li>Challenge & Hackathons</li>
         <li>For Educational Institutions</li>
         <li>About Us</li>
         <li>Contact Us</li>
       </ul>
-      <button className="bg-[#041738] text-white text-[14px]">
+      <button className="bg-[#041738] p-[14px] font-semibold text-white text-[14px] rounded-[10px]">
         Join the Program
       </button>
     </nav>
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,86 @@ export default function Home() {
   return (
     <div className="">
       <NavBar />
+
+      <main className="flex   flex-row items-center justify-center space-x-[10px] mt-[50px] ">
+        <div className="flex flex-col space-y-[50px] text-left w-[450px]">
+          <h2 className="text-[#2B71f0] font-bold text-[30px]">
+            Build Work Experience through Skills Challenges Program
+          </h2>
+          <p>
+            Enhance Your Employability and Accelerate Your Career Growth by
+            working on Hands-on projects & Hackathons from various businesses &
+            organizations
+          </p>
+          <button className="bg-[#2B71f0] w-[300px] p-[10px] rounded-[10px] font-semibold text-white">
+            Get Started
+          </button>
+        </div>
+        <img
+          src="/homePagePhoto.webp"
+          className="h-[400px] w-auto"
+          alt="home image"
+        />
+      </main>
+      <main className="flex flex-col space-y-[20px] pb-[50px] bg-[#F9FAFB] items-center pt-[50px] justify-center">
+        <h2 className="text-[#03192E] text-[30px] font-bold">
+          Experience a New Way of Building Work Experience
+        </h2>
+        <p className="text-gray-300 text-[13px] w-[580px] text-center">
+          Join Skills Challenges Program to accelerate yout career growth and
+          become part of Africa's largest workforce of digital professionals{" "}
+        </p>
+        <div className="flex flex-col w-full px-[200px] space-y-[20px]">
+          <div className="bg-[#2B71F0] space-y-[30px] rounded-[10px] w-full p-[30px] flex flex-col">
+            <img
+              src="/bagLogo.webp"
+              className="w-[60px] rounded-[10px] h-auto"
+              alt="bag logo"
+            />
+            <h2 className="font-bold text-white text-[17px]">
+              Build a Strong Portforio and Hand-On Experience
+            </h2>
+            <p className="text-white">
+              Tackle real-world projects through Challenges and hackathons that
+              mirror real world challenges faced by businesses and
+              organizations.Therefore, showcase your skills and accomplishments
+              to potential employers and clients through a portforio of
+              completed projects
+            </p>
+          </div>
+          <div className="flex flex-row space-x-[20px]">
+            <div className="bg-[#2B71F0] space-y-[30px]  rounded-[10px] w-full p-[30px] flex flex-col">
+              <img
+                src="/bagLogo.webp"
+                className="w-[60px] rounded-[10px] h-auto"
+                alt="bag logo"
+              />
+              <h2 className="font-bold text-white text-[17px]">
+                Enhance Your Employment Path
+              </h2>
+              <p className="text-white">
+                Elop the in-demand skills and build a strong portforio to
+                increase your chances of landing your dream job or contract
+              </p>
+            </div>
+            <div className="bg-[#2B71F0]  space-y-[30px] rounded-[10px] w-full p-[30px] flex flex-col">
+              <img
+                src="/bagLogo.webp"
+                className="w-[60px] rounded-[10px] h-auto"
+                alt="bag logo"
+              />
+              <h2 className="font-bold text-white text-[17px]">
+                Earn Recognition and Prizes
+              </h2>
+              <p className="text-white">
+                Earn both Money and knowledge Prizes by participating in various
+                contests and competitions by working on real world projects and{" "}
+                hackathons from partner companies or organizations
+              </p>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
