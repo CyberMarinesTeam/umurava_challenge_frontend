2bcd69f118ee4dc883979c5b40b5de9d91eef8a0
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2025-01-21 17:00:58 +0200

    adding more

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { TiSocialFacebook } from "react-icons/ti";
+import { AiOutlineGooglePlus } from "react-icons/ai";
 
 const Footer = () => {
-  return <footer className="h-[520px] w-full bg-[#001A40]"></footer>;
+  return (
+    <footer className="h-[520px] w-full bg-[#001A40]  flex flex-col ">
+      <div className="flex flex-row justify-between">
+        <img
+          src="/footerImage.webp"
+          alt="footerImage"
+          className="w-auto h-[80px] "
+        />
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
