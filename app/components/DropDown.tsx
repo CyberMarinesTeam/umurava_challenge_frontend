e68c18f30ea1aa78c7f4f7251d5bd5a34adc83f7
r-yvan <yvankiliye.rubuto@gmail.com>
2025-01-31 10:52:27 +0200

    Commit

--- app/components/DropDown.tsx
+++ app/components/DropDown.tsx
@@ -3,16 +3,23 @@ import { FaChevronDown } from "react-icons/fa6";
 import React, { useState } from "react";
 
 const DropDown = () => {
-  const [r, setR] = useState(false);
+  const [dropped, setDropped] = useState(false);
   return (
-    <div className="flex flex-row gap-2 items-center relative" onClick={() => setR(!r)}>
+    <div
+      className="flex z-50 flex-row gap-2 items-center relative"
+      onClick={() => setDropped(!dropped)}
+    >
       <div>
         <p>This Day</p>
       </div>
       <div>
         <FaChevronDown />
       </div>
-      <div className={`absolute ${ r ? "flex" : "hidden" } bg-[#F9FAFB] rounded-lg top-6 w-32 -right-8 p-1 border border-[#E4E7EC] flex-col`}>
+      <div
+        className={`absolute ${
+          dropped ? "flex" : "hidden"
+        } bg-[#F9FAFB] rounded-lg top-6 w-32 -right-8 p-1 border border-[#E4E7EC] flex-col`}
+      >
         <div className="px-3 py-1 border border-[#FFFFFF00] hover:border-[#F9FAFB] hover:bg-[#f0f3f5] rounded-lg">
           <p>This Week</p>
         </div>
