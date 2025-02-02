714ff10cc222300fda3b12eb4b258bb66035202e
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2025-02-02 13:50:52 +0200

    debugging

--- lib/redux/store.ts
+++ lib/redux/store.ts
@@ -1,13 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { challengeSlice } from "./slices/challengeSlice";
-export const store = configureStore({
-	reducer: {
-		[challengeSlice
-			.reducerPath]: challengeSlice.reducer,
-	},
-//   middleware: (getDefaultMiddleware) => {
-//     getDefaultMiddleware().concat(challengeSlice.middleware);
-//   },
-});
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+
+export const makeStore = () => {
+  return configureStore({
+    reducer: {
+      [challengeSlice.reducerPath]: challengeSlice.reducer,
+    },
+  });
+};
+
+export type AppStore = ReturnType<typeof makeStore>;
+export type RootState = ReturnType<AppStore["getState"]>;
+export type AppDispatch = AppStore["dispatch"];
