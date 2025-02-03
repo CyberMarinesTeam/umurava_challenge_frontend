a113019c70cd5207535dc15d5f295bfa428fb542
Theoneste-dev-dotcom <dufitimanatheoneste05@gmail.com>
2025-02-03 17:58:27 +0200

    changes

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,21 +1,33 @@
-"use client";
-import React from "react";
-import { InfinitySpin, Bars } from "react-loader-spinner";
+// "use client";
+// import React from "react";
+// import { InfinitySpin, Bars } from "react-loader-spinner";
 
-const Loading = () => {
+// const Loading = () => {
+//   return (
+//     <div className="flex justify-center items-center h-screen">
+//       <Bars
+//         height="100"
+//         width="100"
+//         color="#2B71F0"
+//         ariaLabel="bars-loading"
+//         wrapperStyle={{}}
+//         wrapperClass=""
+//         visible={true}
+//       />
+//     </div>
+//   );
+// };
+
+// export default Loading;
+
+import React from 'react'
+
+const loading = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <Bars
-        height="100"
-        width="100"
-        color="#2B71F0"
-        ariaLabel="bars-loading"
-        wrapperStyle={{}}
-        wrapperClass=""
-        visible={true}
-      />
+    <div>
+      fgchjk
     </div>
-  );
-};
+  )
+}
 
-export default Loading;
+export default loading
