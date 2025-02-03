67f3f6973de40ec44acdf8e89f68a967c1a5e809
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2025-02-04 01:02:13 +0200

    protecting the endpoints

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,19 +1,28 @@
 "use client";
 
 import React, { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useSelector } from "react-redux";
+import { RootState } from "@/lib/redux/store";
+import { useGetChallengesQuery } from "@/lib/redux/slices/challengeSlice";
 import SideBar from "@/app/components/SideBar";
 import Header from "@/app/components/Header";
 import ChallengeCard2 from "@/app/components/ChallengeCard2";
-import { useGetChallengesQuery } from "@/lib/redux/slices/challengeSlice";
 import Card from "@/app/components/Card";
-import { FaChevronRight } from "react-icons/fa6";
 import Card2 from "@/app/components/Card2";
-import { useSelector } from "react-redux";
-import { RootState } from "@/lib/redux/store";
-const Page = () => {
-  const user = useSelector((state: RootState) => state.auth.user);
+import { FaChevronRight } from "react-icons/fa6";
 
+const Page = () => {
+  const router = useRouter();
+  const user = useSelector((state: RootState) => state.auth.user);
   const { data } = useGetChallengesQuery();
+
+  useEffect(() => {
+    if (!user) {
+      router.push("/login");
+    }
+  }, [user, router]);
+
   return (
     <div className="excludedDashBoard px-12 py-4">
       <div className="excludedDashBoard h-[56px] flex flex-col gap-[4px] mb-12 mt-[10px]">
@@ -21,22 +30,23 @@ const Page = () => {
           Welcome back, {user?.username}
         </h1>
         <p className="text-[16px] leading-[23px] font-normal text-[#475367]">
-          Build Work Exprience through Skills Challenges
+          Build Work Experience through Skills Challenges
         </p>
       </div>
-      <div className="excludedDashBoard relative h-[316px] ">
-        <div className="grid grid-cols-2  gap-4">
+
+      <div className="excludedDashBoard relative h-[316px]">
+        <div className="grid grid-cols-2 gap-4">
           <Card
             width={540}
-            timeDescription={"This week"}
-            description={"Total Challenges"}
+            timeDescription="This week"
+            description="Total Challenges"
             number={23178}
             percentage={20}
           />
           <Card2
             width={540}
-            timeDescription={"This week"}
-            description={"Total Participants"}
+            timeDescription="This week"
+            description="Total Participants"
             number={231}
             percentage={15}
           />
@@ -44,37 +54,39 @@ const Page = () => {
         <div className="excludedDashBoard grid grid-cols-3 mt-4 gap-4">
           <Card
             width={540}
-            timeDescription={"Last 30 days"}
-            description={"Completed Challenges"}
+            timeDescription="Last 30 days"
+            description="Completed Challenges"
             number={25631}
             percentage={17}
           />
           <Card
             width={540}
-            timeDescription={"Last 20 days"}
-            description={"Open Challenges"}
+            timeDescription="Last 20 days"
+            description="Open Challenges"
             number={56531}
             percentage={16}
           />
           <Card
             width={540}
-            timeDescription={"Last 10 days"}
-            description={"Ongoing Challenges"}
+            timeDescription="Last 10 days"
+            description="Ongoing Challenges"
             number={26631}
             percentage={15}
           />
         </div>
       </div>
-      <div className="excludedDashBoard h-[530px] mt-[50px] ">
-        <div className="excludedDashBoard flex items-center justify-between mb-4 ]">
+
+      <div className="excludedDashBoard h-[530px] mt-[50px]">
+        <div className="excludedDashBoard flex items-center justify-between mb-4">
           <h1 className="text-[18px] leading-[26px] font-semibold text-[#101928]">
             Recent Challenges
           </h1>
-          <button className="text-[#2B71F0] text-[13px] font-normal flex  items-center gap-2  h-[14px] ">
+          <button className="text-[#2B71F0] text-[13px] font-normal flex items-center gap-2 h-[14px]">
             See all <FaChevronRight />
           </button>
         </div>
-        <div className="excludedDashBoard gap-[20px] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-between ">
+
+        <div className="excludedDashBoard gap-[20px] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
           {data?.slice(0, 3).map((challenge) => (
             <ChallengeCard2 key={challenge._id} challenge={challenge} />
           ))}
--- app/talent/challenges/page.tsx
+++ app/talent/challenges/page.tsx
@@ -1,74 +1,117 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { MdOutlineRemoveRedEye } from "react-icons/md";
 import { FiFileText } from "react-icons/fi";
 import SmallStatusCard from "../components/SmallStatusCard";
 import ChallengeCard2 from "@/app/components/ChallengeCard2";
 import { useGetChallengesQuery } from "@/lib/redux/slices/challengeSlice";
+import { useRouter } from "next/navigation";
+import { useSelector } from "react-redux";
+import { RootState } from "@/lib/redux/store";
 
 const Challenges = () => {
-    const { data } = useGetChallengesQuery();
-    const [currentPage, setCurrentPage] = useState(1);
-    const itemsPerPage = 6;
-    
-    const totalPages = data ? Math.ceil(data.length / itemsPerPage) : 1;
-    const startIndex = (currentPage - 1) * itemsPerPage;
-    const paginatedData = data ? data.slice(startIndex, startIndex + itemsPerPage) : [];
+  const router = useRouter();
+  const user = useSelector((state: RootState) => state.auth.user);
 
-    const handleNext = () => {
-        if (currentPage < totalPages) {
-            setCurrentPage(currentPage + 1);
-        }
-    };
+  useEffect(() => {
+    if (!user) {
+      router.push("/login");
+    }
+  }, [user, router]);
 
-    const handlePrevious = () => {
-        if (currentPage > 1) {
-            setCurrentPage(currentPage - 1);
-        }
-    };
+  const { data } = useGetChallengesQuery();
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 6;
 
-    return (
-        <div className="excluded p-[36px] h-full gap-[16px] bg-[#F9FAFB]">
-            <div className="excluded flex flex-row justify-between">
-                <div>
-                    <p className="text-2xl font-bold">Challenges</p>
-                    <p>Join a challenge or a hackathon to gain more valuable work experience</p>
-                </div>
-                <button className="py-[10px] flex h-[55px] flex-row items-center gap-2 px-[18px] bg-[#2B71F0] text-white rounded-lg">
-                    <MdOutlineRemoveRedEye className="w-[24px] h-[24px]" />
-                    View Profile
-                </button>
-            </div>
-            <div className="py-[16px] excluded flex w-full items-center">
-                <div className="flex justify-start gap-[10px] border-b pb-5 w-full">
-                    <SmallStatusCard count={0} icon={<FiFileText />} text="All Challenges" />
-                    <SmallStatusCard count={0} icon={<FiFileText />} text="Completed Challenge" />
-                    <SmallStatusCard count={0} icon={<FiFileText />} text="Open Challenge" />
-                    <SmallStatusCard count={0} icon={<FiFileText />} text="Ongoing Challenge" />
-                </div>
-            </div>
-            <div className="flex flex-wrap gap-[20px]">
-                {paginatedData.map((challenge) => (
-                    <ChallengeCard2 key={challenge._id} challenge={challenge} />
-                ))}
-            </div>
-            <div className="flex justify-between py-10 px-10">
-                <button 
-                    onClick={handlePrevious} 
-                    disabled={currentPage === 1} 
-                    className={`px-3 py-2 text-sm w-[96px] h-[36px] rounded-md ${currentPage === 1 ? "bg-gray-300 text-gray-500" : "bg-white text-[#98A2B3] active:bg-[#2B71F0] active:text-white"}`}>
-                    Previous
-                </button>
-                <button 
-                    onClick={handleNext} 
-                    disabled={currentPage === totalPages} 
-                    className={`px-3 py-2 text-sm w-[96px] h-[36px] rounded-md ${currentPage === totalPages ? "bg-gray-300 text-gray-500" : "bg-white text-[#98A2B3] active:bg-[#2B71F0] active:text-white"}`}>
-                    Next
-                </button>
-            </div>
+  const totalPages = data ? Math.ceil(data.length / itemsPerPage) : 1;
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const paginatedData = data
+    ? data.slice(startIndex, startIndex + itemsPerPage)
+    : [];
+
+  const handleNext = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handlePrevious = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  return (
+    <div className="excluded p-[36px] h-full gap-[16px] bg-[#F9FAFB]">
+      <div className="excluded flex flex-row justify-between">
+        <div>
+          <p className="text-2xl font-bold">Challenges</p>
+          <p>
+            Join a challenge or a hackathon to gain more valuable work
+            experience
+          </p>
         </div>
-    );
+        <button className="py-[10px] flex h-[55px] flex-row items-center gap-2 px-[18px] bg-[#2B71F0] text-white rounded-lg">
+          <MdOutlineRemoveRedEye className="w-[24px] h-[24px]" />
+          View Profile
+        </button>
+      </div>
+      <div className="py-[16px] excluded flex w-full items-center">
+        <div className="flex justify-start gap-[10px] border-b pb-5 w-full">
+          <SmallStatusCard
+            count={0}
+            icon={<FiFileText />}
+            text="All Challenges"
+          />
+          <SmallStatusCard
+            count={0}
+            icon={<FiFileText />}
+            text="Completed Challenge"
+          />
+          <SmallStatusCard
+            count={0}
+            icon={<FiFileText />}
+            text="Open Challenge"
+          />
+          <SmallStatusCard
+            count={0}
+            icon={<FiFileText />}
+            text="Ongoing Challenge"
+          />
+        </div>
+      </div>
+      <div className="flex flex-wrap gap-[20px]">
+        {paginatedData.map((challenge) => (
+          <ChallengeCard2 key={challenge._id} challenge={challenge} />
+        ))}
+      </div>
+      <div className="flex justify-between py-10 px-10">
+        <button
+          onClick={handlePrevious}
+          disabled={currentPage === 1}
+          className={`px-3 py-2 text-sm w-[96px] h-[36px] rounded-md ${
+            currentPage === 1
+              ? "bg-gray-300 text-gray-500"
+              : "bg-white text-[#98A2B3] active:bg-[#2B71F0] active:text-white"
+          }`}
+        >
+          Previous
+        </button>
+        <button
+          onClick={handleNext}
+          disabled={currentPage === totalPages}
+          className={`px-3 py-2 text-sm w-[96px] h-[36px] rounded-md ${
+            currentPage === totalPages
+              ? "bg-gray-300 text-gray-500"
+              : "bg-white text-[#98A2B3] active:bg-[#2B71F0] active:text-white"
+          }`}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Challenges;
