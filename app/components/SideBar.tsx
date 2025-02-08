e6b31a45871a4c56acad714bfca37a88c1051611
r-yvan <yvankiliye.rubuto@gmail.com>
2025-02-08 21:07:29 +0200

    Commit

--- app/components/SideBar.tsx
+++ app/components/SideBar.tsx
@@ -43,35 +43,35 @@ const SideBar = () => {
   }, [user, router]);
 
   return (
-    <div className="w-[272px] bg-[#2b71F0] text-white px-2 h-[1400px]">
+    <div className="w-[272px] max-[1000px]:w-[64px] bg-[#2b71F0] text-white px-2 h-[1400px]">
       <div className="flex flex-col justify-between">
         <img
           src="/umuravaLogo2.webp"
           alt="User"
           draggable={false}
-          className="w-[51px] h-[35px] mt-[20px] ml-[30px] rounded-3"
+          className="w-[51px] max-[1000px]:w-full max-[1000px]:h-auto h-[35px] mt-[20px] max-[1000px]:ml-0 max-[1000px]:mt-5 ml-[30px] rounded-3"
         />
         <div className="mb-[473px] mt-[30px]">
           <ul>
             <Link
               href="/admin/dashboard"
-              className={`mb-[2px] p-4 flex items-center gap-2 w-[256px] hover:bg-white transition-all ease-in-out duration-300 hover:text-blue-600 h-[44px] rounded-sm ${
+              className={`mb-[2px] max-[1000px]:w-[48px] p-4 flex items-center gap-2 w-[256px] hover:bg-white transition-all ease-in-out duration-300 hover:text-blue-600 h-[44px] rounded-sm ${
                 isActive("/admin/dashboard") ? "bg-white text-blue-600" : ""
               }`}
             >
               <GoHome className="text-[20px]" />
-              <span className="text-[14px] font-normal leading-5">
+              <span className="text-[14px] max-[1000px]:hidden font-normal leading-5">
                 Dashboard
               </span>
             </Link>
             <Link
               href="/admin/challenges"
-              className={`mb-[2px] p-4 flex items-center gap-2 w-[256px] hover:bg-white transition-all ease-in-out duration-300 hover:text-blue-600 h-[44px] rounded-sm ${
+              className={`mb-[2px] p-4 max-[1000px]:w-[48px] flex items-center gap-2 w-[256px] hover:bg-white transition-all ease-in-out duration-300 hover:text-blue-600 h-[44px] rounded-sm ${
                 isActive("/admin/challenges") ? "bg-white text-blue-600" : ""
               }`}
             >
               <IoDocumentTextOutline className="text-[20px]" />
-              <span className="text-[14px] font-normal leading-5">
+              <span className="text-[14px] max-[1000px]:hidden font-normal leading-5">
                 Challenges
               </span>
             </Link>
@@ -82,10 +82,10 @@ const SideBar = () => {
                   (modal as HTMLDialogElement).showModal();
                 }
               }}
-              className="mb-[2px] p-4 flex items-center gap-2 w-[256px] hover:bg-white transition-all ease-in-out duration-300 hover:text-blue-600 h-[44px] rounded-sm"
+              className="mb-[2px] p-4 max-[1000px]:w-[48px] flex items-center gap-2 w-[256px] hover:bg-white transition-all ease-in-out duration-300 hover:text-blue-600 h-[44px] rounded-sm"
             >
               <IoPersonAddOutline className="text-[20px]" />
-              <span className="text-[14px] font-normal leading-5">
+              <span className="text-[14px] max-[1000px]:hidden font-normal leading-5">
                 Community
               </span>
             </button>
@@ -120,41 +120,41 @@ const SideBar = () => {
           <ul>
             <Link
               href="/admin/dashboard"
-              className="mb-[2px] p-4 flex items-center gap-2 w-[256px] hover:bg-white transition-all ease-in-out duration-300 hover:text-blue-600 h-[44px] rounded-sm"
+              className="mb-[2px] p-4 max-[1000px]:w-[48px]  flex items-center gap-2 w-[256px] hover:bg-white transition-all ease-in-out duration-300 hover:text-blue-600 h-[44px] rounded-sm"
             >
               <IoSettingsOutline className="text-[20px]" />
-              <span className="text-[14px] font-normal leading-5">
+              <span className="text-[14px] max-[1000px]:hidden font-normal leading-5">
                 Settings
               </span>
             </Link>
             <Link
               href="/admin/challenges"
-              className="mb-[2px] p-4 flex items-center gap-2 w-[256px] hover:bg-white transition-all ease-in-out duration-300 hover:text-blue-600 h-[44px] rounded-sm"
+              className="mb-[2px] p-4 max-[1000px]:w-[48px] flex items-center gap-2 w-[256px] hover:bg-white transition-all ease-in-out duration-300 hover:text-blue-600 h-[44px] rounded-sm"
             >
               <IoHeadsetOutline className="text-[20px]" />
-              <span className="text-[14px] font-normal leading-5">
+              <span className="text-[14px] max-[1000px]:hidden font-normal leading-5">
                 Help Center
               </span>
             </Link>
             <Link
               href="/admin/community"
-              className="mb-[2px] p-4 flex items-center gap-2 w-[256px] hover:bg-white transition-all ease-in-out duration-300 hover:text-blue-600 h-[44px] rounded-sm"
+              className="mb-[2px] p-4 max-[1000px]:w-[48px] flex items-center gap-2 w-[256px] hover:bg-white transition-all ease-in-out duration-300 hover:text-blue-600 h-[44px] rounded-sm"
             >
               <IoGiftOutline className="text-[20px]" />
-              <span className="text-[14px] font-normal leading-5">
+              <span className="text-[14px] max-[1000px]:hidden font-normal leading-5">
                 Refer family & friends
               </span>
             </Link>
           </ul>
         </div>
-        <div className="pl-[10px] flex flex-col space-y-[20px]">
+        <div className="pl-[10px] max-[1000px]:pl-0 flex max-[1000px]:w-full max-[1000px]:items-center max-[1000px]:justify-center flex-col space-y-[20px]">
           <div className="flex flex-row space-x-[7px]">
             <img
               src="/profile2.webp"
               alt="profileImage"
               className="h-[40px] object-cover border-[2px] border-white w-[40px] rounded-full"
             />
-            <div className="flex  flex-col gap-1 w-[80%]">
+            <div className="flex max-[1000px]:hidden flex-col gap-1 w-[80%]">
               <p className="text-[12px] font-normal leading-5">
                 {user?.username}, PM
               </p>
